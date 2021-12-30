1359bae8205917bfbf9687599e570bb60d03cf26
Your Name <you@example.com>
2021-12-30 10:58:54 +0800

    1230-1

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -24,21 +24,19 @@ public class UserController {
         return "users";
     }
 
-
-
-    @GetMapping("/users/new")
-    public String showNewFrom(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("pageTitle", "Add New User");
-        return "user_from";
-    }
-
     @GetMapping("/users/member")
     public String showMember(Model model) {
         List<Member> memberall = service.memberAll();
         //model.addAttribute("user", new Member());
         model.addAttribute("memberall", "memberall");
         return "member";
+    }
+
+    @GetMapping("/users/new")
+    public String showNewFrom(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
+        return "user_from";
     }
 
     @PostMapping("/users/save")
--- src/main/java/com/mycompany/user/UserService.java
+++ src/main/java/com/mycompany/user/UserService.java
@@ -17,7 +17,9 @@ public class UserService {
         return (List<User>) repo.findAll();
     }
 
-    public List<Member> memberAll(){ return (List<Member>) r.findAll(); }
+    public List<Member> memberAll(){
+        return (List<Member>) r.findAll();
+    }
 
     public void save(User user) {
         repo.save(user);
